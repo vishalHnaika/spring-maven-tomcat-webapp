cd10c53aac8426b9fbad511c83bc36ecca189e4c
bipin115 <bipin.singh4115@gmail.com>
2021-07-04 16:38:37 +0530

    Added Src files to the project

---
+++ src/main/java/com/simplilearn/webapp/springmavenapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.simplilearn.webapp.springmavenapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringMavenAppApplication.class);
+	}
+
+}
---
+++ src/main/java/com/simplilearn/webapp/springmavenapp/SpringMavenAppApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.webapp.springmavenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavenAppApplication.class, args);
+	}
+
+}
